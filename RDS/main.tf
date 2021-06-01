ee73fa1a2991282fbd34f52c28ea2957351357cf
Shruti sharma <krati465@gmail.com>
2021-06-01 16:56:19 +0530

    Create main.tf

---
+++ RDS/main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_version = "= 0.15.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 3.7.0"
+    }
+  }
+}
+
+resource "aws_db_instance" "mysql" {
+  engine         = "mysql"
+  engine_version = "5.6.41"
+
+  name     = var.name
+  username = var.master_username
+  password = var.master_password
+
+  instance_class    = var.instance_class
+  allocated_storage = var.allocated_storage
+  storage_type      = var.storage_type
+
+  
+}
